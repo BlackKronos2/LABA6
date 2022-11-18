83fa653bae2063024a70ca58aec3a99f9b7faa35
BlackKronos2 <arefev.arefev.kirill@mail.ru>
2022-11-18 23:19:04 +0700

    Изменения исключений окна ввода

--- Form1.cs
+++ Form1.cs
@@ -20,6 +20,8 @@ namespace LABA6
         {
             try
             {
+                TextBox[] textBoxes = { textBox1, textBox2, textBox3, textBox4, textBox5, textBox6, textBox7, textBox8, textBox9 };
+
                 int mx_hlth_wr = Convert.ToInt32(textBox1.Text);
                 int mx_hlth_hlr = Convert.ToInt32(textBox2.Text);
                 int imp_strngth1 = Convert.ToInt32(textBox3.Text);
@@ -30,50 +32,16 @@ namespace LABA6
                 int amount_war = Convert.ToInt32(textBox8.Text);
                 int amount_hlr = Convert.ToInt32(textBox9.Text);
 
-
-                TextBox[] textBoxes = { textBox1, textBox2, textBox3, textBox4, textBox5, textBox6, textBox7, textBox8, textBox9 };
+                foreach (TextBox box in textBoxes) {
+                    if (Convert.ToInt32(box) < 0 || box.Text == string.Empty) {
+                        box.Text = "Ошибка";
+                    }
+                }
 
                 if (amount_war == 0 && amount_hlr == 0 || amount_war < 0 || amount_hlr < 0)
                 {
                     throw new Exception("Много");
-                }
-
-                if (mx_hlth_wr < 0)
-                {
-                    throw new Exception("Много");
-                }
-
-                if (mx_hlth_hlr < 0)
-                {
-                    throw new Exception("Много");
-                }
-
-                if (imp_strngth1 < 0)
-                {
-                    throw new Exception("Много");
-                }
-
-                if (imp_strngth2 < 0)
-                {
-                    throw new Exception("Много");
-                }
-
-                if (imp_hlr < 0)
-                {
-                    throw new Exception("Много");
-                }
-
-                if (rec1 < 0)
-                {
-                    throw new Exception("Много");
-                }
-
-                if (rec2 < 0)
-                {
-                    throw new Exception("Много");
-                }
-
-
+                }         
 
                 Group group1 = new Group(mx_hlth_wr, mx_hlth_hlr, imp_strngth1, imp_strngth2, imp_hlr, rec1, rec2, amount_war, amount_hlr);
                 Group group2 = new Group(mx_hlth_wr, mx_hlth_hlr, imp_strngth1, imp_strngth2, imp_hlr, rec1, rec2, amount_war, amount_hlr);
@@ -82,19 +50,10 @@ namespace LABA6
                 Form2.Show();
                 this.Hide();
 
-
             }
             catch
             {
-                textBox1.Text = "Ошибка!";
-                textBox2.Text = "Ошибка!";
-                textBox3.Text = "Ошибка!";
-                textBox4.Text = "Ошибка!";
-                textBox5.Text = "Ошибка!";
-                textBox6.Text = "Ошибка!";
-                textBox7.Text = "Ошибка!";
-                textBox8.Text = "Ошибка!";
-                textBox9.Text = "Ошибка!";
+
             }
         }
     }
