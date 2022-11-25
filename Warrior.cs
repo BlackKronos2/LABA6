f7ca76827f54b574d54c7b93531939a482b69eb5
BlackKronos2 <arefev.arefev.kirill@mail.ru>
2022-11-25 15:42:10 +0700

    feature

--- Warrior.cs
+++ Warrior.cs
@@ -8,6 +8,24 @@ namespace LABA6
 {
     public class Warrior: Charapter
     {
+        public Warrior()
+        {
+            this.name = "";
+            this.alive = false;
+            this.health = 0;
+            this.max_health = 0;
+            this.impact_strength = 0;
+            this.recovery = 0;
+        }
+        public Warrior(string n, int max_h, int im_str, int rec)
+        {
+            name = n;
+            max_health = max_h;
+            health = max_h;
+            impact_strength = im_str;
+            recovery = rec;
+            alive = true;
+        }
         override public int Strike_at_war()
         {
             return impact_strength;
