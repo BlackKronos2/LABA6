da28982f4302916592cc1dedc60e846c1f4e96bf
BlackKronos2 <arefev.arefev.kirill@mail.ru>
2022-11-25 15:40:26 +0700

    Абстракция 2

--- Healer.cs
+++ Healer.cs
@@ -6,39 +6,8 @@ using System.Threading.Tasks;
 
 namespace LABA6
 {
-    public class Healer : Warrior
+    public class Healer : Charapter
     {
-        private int impact_treatment;
-        public Healer()
-        {
-            this.impact_treatment = 0;
-        }
-        public Healer(string n, int max_h, int im_str, int im_treat, int rec)
-        {
-            name = n;
-            max_health = max_h;
-            health = max_health;
-            impact_strength = im_str;
-            impact_treatment = im_treat;
-            recovery = rec;
-            alive = true;
-        }
-        public int Impact_treatment
-        {
-            set
-            {
-                if (value <= 0)
-                {
-                    Console.WriteLine("Значение силы исцеления персонажа-целителя меньше или равно 0");
-                }
-                else
-                {
-                    this.impact_treatment = value;
-                }
-            }
-            get { return impact_strength; }
-        }
-
         override public int Strike_at_war()
         {
             return impact_strength;
